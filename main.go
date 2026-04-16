11debfee2fbbdd28b06eb63605f2a3d44c01824c
k4rkie <karkeekrish07@gmail.com>
2026-04-16 18:56:28 +0545

    Added arguments for <filename> and <keyword>. Logging the lines to the stdout

--- main.go
+++ main.go
@@ -1,24 +1,54 @@
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
-	fmt.Println("===Khoj===")
 
-	// get the file name as an arg
-	file_name := os.Args[1]
+	// get the file name as an arg -f
+	filename := flag.String("f", "", "File to search")
 
-	// read data into a buffer
-	data_buff, err := os.ReadFile(file_name)
+	// get the keyword to search for as an arg -k
+	keyword := flag.String("k", "", "Term to search for")
 
+	// parse the arguments
+	flag.Parse()
+
+	if *filename == "" || *keyword == "" {
+		fmt.Println("Usage: khoj -f <file> -k <keyword>")
+		os.Exit(1)
+	}
+
+	// open the file to read
+	file, err := os.Open(*filename)
 	if err != nil {
 		log.Fatal(err)
 	}
+	// Close on exit
+	defer file.Close()
 
-	file_data_text := string(data_buff)
-	fmt.Print(file_data_text)
+	// create a file scanner
+	scanner := bufio.NewScanner(file)
+	// track file number
+	line_number := 0
+	for scanner.Scan() {
+		// starting index of keyword if exists
+		keyword_index := strings.Index(scanner.Text(), *keyword)
+
+		line_number++
+		// if keyword exists print it from its starting index
+		if keyword_index != -1 {
+			fmt.Printf("[Line: %d]. \033[1;32m%s\033[0m\n", line_number, scanner.Text()[keyword_index:])
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
