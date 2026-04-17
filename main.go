18f107de70293668931f87617c43efde1649b147
k4rkie <karkeekrish07@gmail.com>
2026-04-17 10:41:02 +0545

    Added DirWalk, now looks for every file in a provided root dir via -d flag

--- main.go
+++ main.go
@@ -6,48 +6,63 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
 func main() {
 
 	// get the file name as an arg -f
-	filename := flag.String("f", "", "File to search")
-
+	dirName := flag.String("d", ".", "Directory to search in")
 	// get the keyword to search for as an arg -k
-	keyword := flag.String("k", "", "Term to search for")
-
+	keyword := flag.String("k", "TODO", "Term to search for")
 	// parse the arguments
 	flag.Parse()
 
-	if *filename == "" || *keyword == "" {
-		fmt.Println("Usage: khoj -f <file> -k <keyword>")
+	if *dirName == "" || *keyword == "" {
+		fmt.Println("Usage: khoj -d <dir> -k <keyword>")
 		os.Exit(1)
 	}
 
-	// open the file to read
-	file, err := os.Open(*filename)
+	err := filepath.WalkDir(*dirName, func(path string, d os.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if !d.IsDir() {
+			searchInFile(path, *keyword)
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Printf("Error walking the path %q: %v\n", *dirName, err)
+	}
+}
+
+func searchInFile(path string, keyword string) {
+	file, err := os.Open(path)
+
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Close on exit
+
 	defer file.Close()
 
-	// create a file scanner
 	scanner := bufio.NewScanner(file)
-	// track file number
+
 	line_number := 0
 	for scanner.Scan() {
 		// starting index of keyword if exists
-		keyword_index := strings.Index(scanner.Text(), *keyword)
+		keyword_index := strings.Index(scanner.Text(), keyword)
 
 		line_number++
 		// if keyword exists print it from its starting index
 		if keyword_index != -1 {
-			fmt.Printf("[Line: %d]. \033[1;32m%s\033[0m\n", line_number, scanner.Text()[keyword_index:])
+			fmt.Printf("[File: %s] [Line: %d]. \033[1;32m%s\033[0m\n", path, line_number, scanner.Text()[keyword_index:])
 		}
 	}
 
+	// if error occurs while reading then exit
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
